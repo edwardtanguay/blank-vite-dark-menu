71339b1da7578a373e9a08cc81e90b12faaa8b6b
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-30 14:18:46 +0100

    returned to original

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,6 @@ import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
 import { PageWelcome } from './pages/PageWelcome';
 import { PageInfo } from './pages/PageInfo';
 import { PageAbout } from './pages/PageAbout';
-import { PageProduct } from './pages/PageProduct';
-
-const products = [
-	{
-		id: 1,
-		title: "The First Product"
-	},
-	{
-		id: 2,
-		title: "The Second Product"
-	}
-];
 
 function App() {
 	return (
@@ -22,16 +10,13 @@ function App() {
 			<h1>Info Site</h1>
 			<nav>
 				<NavLink to="/welcome">Welcome</NavLink>
-				<NavLink to="/info">Brands</NavLink>
+				<NavLink to="/info">Info</NavLink>
 				<NavLink to="/about">About</NavLink>
 			</nav>
 
 			<Routes>
 				<Route path="/welcome" element={<PageWelcome />} />
 				<Route path="/info" element={<PageInfo />} />
-				<Route path="/product" element={<PageProduct products={products} />}>
-					<Route path=":id" element={null} />
-				</Route>
 				<Route path="/about" element={<PageAbout />} />
 				<Route path="/" element={<Navigate to="/welcome" replace />} />
 			</Routes>
